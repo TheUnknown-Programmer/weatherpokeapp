24c5c8bc54c03bbc4739517b9700ba79461c04b3
f.jonathan9700@gmail.com <106790970+TheUnknown-Programmer@users.noreply.github.com>
2023-12-12 21:50:11 -0500

    Folder Structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 'use client';
 
 import React from 'react';
+import App from './weatherapp/page'
 
-import Hero from '../components/Hero';
-import Content from '../components/Content';
+
 
 export default function Index() {
   return (
     <>
-      <Hero />
-      <hr />
-      <Content />
+      <App></App>
     </>
   );
 }
